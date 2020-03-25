0470ff404efae4fde5a5e162b1cd016d6b96b4b2
Vincent Baron <vincentbaron1996@gmail.com>
2020-03-25 11:10:52 +0000

    t6

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -10,6 +10,7 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
     x2 = (unsigned char *)s2;
     if (n == 0)
         return (0);
+    i = 0;
     while (i < n)
     {
         if (*x1 != *x2)
