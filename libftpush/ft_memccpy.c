c9c4fe3aada3697aaec8e6eac7cc86b8fde42efb
VincentBaron <vincentbaron1996@gmail.com>
2020-05-03 17:26:59 +0200

    memcpy4

--- libftpush/ft_memccpy.c
+++ libftpush/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 13:13:35 by vbaron            #+#    #+#             */
-/*   Updated: 2020/05/03 17:25:14 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/05/03 17:26:50 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 		return ((void *)0);
 	while (n > 0)
 	{
-		*((char *)dest) = *((char *)src);
-		if (*((char *)src) == (unsigned char)c)
-			return (dest + 1);
+		*((unsigned char *)dest) = *((unsigned char *)src);
+		if (*((unsigned char *)src) == (unsigned char)c)
+			return ((unsigned char *)dest + 1);
 		dest++;
 		src++;
 		n--;
