6bd39dfe834d7039a39350a3272a45d5c683e2a3
VincentBaron <vincentbaron1996@gmail.com>
2020-05-03 19:56:27 +0200

    strlcpy up6

--- libftpush/ft_strtrim.c
+++ libftpush/ft_strtrim.c
@@ -6,56 +6,28 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 13:16:32 by vbaron            #+#    #+#             */
-/*   Updated: 2020/05/03 19:50:21 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/05/03 19:56:15 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		check_charset(char c, char const *set)
+char	*ft_strtrim(const char *s1, char const *set)
 {
-	while (*set)
-	{
-		if (*set == c)
-			return (1);
-		set++;
-	}
-	return (0);
-}
+	size_t	x;
+	size_t	y;
 
-char	*ft_strndup(char *temp, int n)
-{
-	char	*line;
-	int		i;
-
-	if (!(line = (char *)malloc(sizeof(char) * (n + 1))))
+	if (!s1)
 		return (NULL);
-	i = 0;
-	while (i < n)
-	{
-		line[i] = temp[i];
-		i++;
-	}
-	line[i] = '\0';
-	temp = NULL;
-	free(temp);
-	return (line);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	int		i;
-	int		x;
-	char	*trim;
-
-	i = 0;
-	while (check_charset(s1[i], set) == 1)
-		i++;
-	trim = ft_strdup(&s1[i]);
-	x = ft_strlen(trim);
-	x--;
-	while (check_charset(trim[x], set) == 1)
-		x--;
-	trim = ft_strndup(trim, x + 1);
-	return (trim);
+	if (!set)
+		return (ft_strdup((char *)s1));
+	x = 0;
+	y = ft_strlen(s1) - 1;
+	while (s1[x] && ft_strchr(set, s1[x]))
+		x++;
+	if (!s1[x])
+		return (ft_strdup(""));
+	while (s1[y] && ft_strchr(set, s1[y]))
+		j--;
+	return (ft_substr(s1, x, y - x + 1));
 }
