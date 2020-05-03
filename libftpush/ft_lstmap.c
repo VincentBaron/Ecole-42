7a44f8d9d0543d5daaebace96b9116759101d940
VincentBaron <vincentbaron1996@gmail.com>
2020-05-03 23:59:53 +0200

    strlcpy up6

--- libftpush/ft_lstmap.c
+++ libftpush/ft_lstmap.c
@@ -6,43 +6,28 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 13:13:11 by vbaron            #+#    #+#             */
-/*   Updated: 2020/05/03 23:57:00 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/05/03 23:59:45 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list	*new;
-
-	(void)del;
-	if (lst)
-	{
-		new = f(lst);
-		new->next = ft_lstmap(lst->next, f);
-		return (new);
-	}
-	return (NULL);
-}*/
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*map;
-	t_list	*tmp;
+	t_list	*new;
+	t_list	*head;
 
-	if (lst == NULL || f == NULL)
+	(void)del;
+	if (!f || !del)
 		return (NULL);
-	map = NULL;
-	while (lst != NULL)
+	if(!(head = malloc(sizeof(char) * ft_lstsize(lst))))
+		return (NULL);
+	new = head;
+	while (lst)
 	{
-		if ((tmp = ft_lstnew((*f)(lst->content))) == NULL)
-		{
-			ft_lstclear(&map, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&map, tmp);
+		new = lst_new(f(lst));
 		lst = lst->next;
+		new = new->next;
 	}
-	return (map);
+	return (head);
 }
