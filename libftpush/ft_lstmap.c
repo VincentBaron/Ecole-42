4db7b036ee665e461868c0d8ffab38e6e2073e98
VincentBaron <vincentbaron1996@gmail.com>
2020-05-03 23:57:05 +0200

    strlcpy up6

--- libftpush/ft_lstmap.c
+++ libftpush/ft_lstmap.c
@@ -6,13 +6,13 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 13:13:11 by vbaron            #+#    #+#             */
-/*   Updated: 2020/05/03 23:54:21 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/05/03 23:57:00 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+/*t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 
@@ -24,4 +24,25 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		return (new);
 	}
 	return (NULL);
+}*/
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*map;
+	t_list	*tmp;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	map = NULL;
+	while (lst != NULL)
+	{
+		if ((tmp = ft_lstnew((*f)(lst->content))) == NULL)
+		{
+			ft_lstclear(&map, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&map, tmp);
+		lst = lst->next;
+	}
+	return (map);
 }
